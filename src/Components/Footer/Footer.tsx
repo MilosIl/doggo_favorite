81e164eb2da2f5ab6ea2cf384bb7728b2e393507
Milos <milos.ilic20@hotmail.com>
2023-01-17 16:20:01 +0100

    useDebounce

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -5,7 +5,7 @@ export interface IFooter{
 const Footer: React.FC<IFooter> = () => {
   return (
     <footer className="bottom-0 w-full bg-gray-200 text-zinc-800 text-center py-4 font-bold">
-      Made by Milos I. 2022 ©{' '}
+      Made by Milos I. 2023 ©
     </footer>
   );
 };
