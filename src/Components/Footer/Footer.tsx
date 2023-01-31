b256b9a512b238aee45f54ff40c4b63ee8fa5df9
Milos <milos.ilic20@hotmail.com>
2023-01-31 18:01:10 +0100

    updated footer

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -3,7 +3,7 @@ export interface IFooter {}
 const Footer: React.FC<IFooter> = () => {
   return (
     <footer className="bottom-0 w-full bg-gray-200 text-zinc-800 text-center py-4 font-bold">
-      Made by Mines-dev. 2023 ©
+      Made by <a className="no-underline hover:cursor-pointer" href="www.mines-dev.com">Mines-dev</a> 2023 © 
     </footer>
   );
 };
