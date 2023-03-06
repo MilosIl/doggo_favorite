1192b471298de87e14542a0871dfa90f9c3b4be4
Milos <milos.ilic20@hotmail.com>
2023-03-06 14:14:12 +0100

    container width

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,17 +3,14 @@ import SearchBar from '../Components/SearchBar/SearchBar';
 import FavoriteList from '../Components/FavoriteList/FavoriteList';
 import Footer from '../Components/Footer/Footer';
 
-import {FavoriteContext} from '../context/FavoriteContext'
-
-
+// import {FavoriteContext} from '../context/FavoriteContext'
 
 export default function Home() {
-
   return (
-    <FavoriteContext.Provider value={FavoriteContext}>
-      <div className="min-h-screen">
-        <FavoriteList  />
-        <div className="flex flex-col justify-center items-center mx-auto">
+    <div className="relative container mx-auto">
+      <div className="flex min-h-screen">
+        <FavoriteList />
+        <div className="flex flex-col items-center mx-auto">
           <h1 className="text-2xl font-extrabold  text-zinc-800">
             Welcome to Doggo site
           </h1>
@@ -26,6 +23,6 @@ export default function Home() {
         </div>
       </div>
       <Footer />
-    </FavoriteContext.Provider>
+    </div>
   );
 }
